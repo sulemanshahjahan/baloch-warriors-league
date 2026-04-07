fbe9002ac6a67406e89d4e24cb82a24bc5984333
Suleman <suleman.shahjahan@gmail.com>
2026-04-07 21:23:41 +0300

    fix: restore slugify export that was accidentally removed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,17 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import slugifyLib from "slugify";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function slugify(text: string): string {
+  return slugifyLib(text, {
+    lower: true,
+    strict: true,
+    trim: true,
+  });
 }
 
 export type GameCategory =
